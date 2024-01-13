31759965539ee057d419d3ca8af7605167c55e64
Deep Majumder <deep.majumder2019@gmail.com>
2024-01-13 20:35:46 +0530

    Made the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { Box, Container, Grid, Typography } from "@mui/material";
 import Months from "./Months";
 
 function App() {
   return (
-    <>
-      <Months />
-    </>
+    <Box mt={5}>
+      <Container>
+        <Grid container alignItems="center" spacing={5}>
+          <Grid item xs={3} />
+          <Grid item xs={6}>
+            <Typography align="center" variant="h2">
+              Chess.com Games
+            </Typography>
+          </Grid>
+          <Grid item xs={3} />
+
+          <Grid item xs={2} />
+          <Grid item xs={8}>
+            <Months />
+          </Grid>
+          <Grid item xs={2} />
+        </Grid>
+      </Container>
+    </Box>
   );
 }
 
--- src/Months.jsx
+++ src/Months.jsx
@@ -1,62 +1,64 @@
-import { Alert, Link, Paper, Table, TableCell, TableContainer, TableRow, TableHead, TableBody } from "@mui/material";
+import { Box, Alert, Link, Table, TableCell, TableContainer, TableRow, TableHead, TableBody, Paper } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Months() {
-    const [months, setMonths] = useState([]);
-    const [fetchFailed, setFetchFailed] = useState(null);
+  const [months, setMonths] = useState([]);
+  const [fetchFailed, setFetchFailed] = useState(null);
 
-    useEffect(() => {
-        const url = 'https://cc2lc.chess.reddocmd.dev/months';
-        axios.get(url)
-            .then((resp) => {
-                setFetchFailed(null);
-                setMonths(resp.data);
-            })
-            .catch((err) => {
-                setFetchFailed(err);
-            });
-    }, []);
+  useEffect(() => {
+    const url = 'https://cc2lc.chess.reddocmd.dev/months';
+    axios.get(url)
+      .then((resp) => {
+        setFetchFailed(null);
+        setMonths(resp.data);
+      })
+      .catch((err) => {
+        setFetchFailed(err);
+      });
+  }, []);
 
-    if (fetchFailed !== null) {
-        return (
-            <Alert severity="error">
-                Failed to fetch month list
-            </Alert>
-        );
-    } else {
-        const monthNames = [
-            'January', 'February', 'March', 'April',
-            'May', 'June', 'July', 'August',
-            'September', 'October', 'November', 'December'
-        ];
-        const rows = months.map(({ month, year }, idx) => {
-            const monthName = monthNames[month - 1];
-            return (
-                <TableRow key={idx}>
-                    <TableCell align="left">{monthName}</TableCell>
-                    <TableCell align="left">{year}</TableCell>
-                    <TableCell align="left">
-                        <Link href="#">Games</Link>
-                    </TableCell>
-                </TableRow>
-            );
-        });
-        return (
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="left">Month</TableCell>
-                            <TableCell align="left">Year</TableCell>
-                            <TableCell align="left">Link</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        );
-    }
+  if (fetchFailed !== null) {
+    return (
+      <Alert severity="error">
+        Failed to fetch month list
+      </Alert>
+    );
+  } else {
+    const monthNames = [
+      'January', 'February', 'March', 'April',
+      'May', 'June', 'July', 'August',
+      'September', 'October', 'November', 'December'
+    ];
+    const rows = months.map(({ month, year }, idx) => {
+      const monthName = monthNames[month - 1];
+      return (
+        <TableRow key={idx}>
+          <TableCell align="left">{monthName}</TableCell>
+          <TableCell align="left">{year}</TableCell>
+          <TableCell align="left">
+            <Link href="#">Games</Link>
+          </TableCell>
+        </TableRow>
+      );
+    });
+    return (
+      <Box>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">Month</TableCell>
+                <TableCell align="left">Year</TableCell>
+                <TableCell align="left">Link</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    );
+  }
 }
